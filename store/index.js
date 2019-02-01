d571d352b57935e9ce3fca43095990af80765c2d
Michael Leung <mleung06@gmail.com>
2019-01-31 22:40:08 -0500

    New entries update redux store in realtime

---
+++ store/index.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+
+import { wordReducer } from './words';
+
+const reducer = combineReducers({ words: wordReducer });
+const middleware = applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }));
+const store = createStore(reducer, middleware);
+
+export default store;
---
+++ store/words.js
@@ -0,0 +1,25 @@
+const GOT_WORDS = 'GOT_WORDS';
+
+const gotWords = (words) => ({
+  type: GOT_WORDS,
+  words
+});
+
+export const updateWords = (words) => {
+  return async (dispatch) => {
+    dispatch(gotWords(words));
+  }
+}
+
+const initialState = {
+  words: []
+}
+
+export const wordReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GOT_WORDS:
+      return { ...state, words: action.words }
+    default:
+      return state;
+  }
+}
