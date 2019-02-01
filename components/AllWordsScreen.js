8809b8b70702f4c692538e7d6eb2ca90cf06169d
Milly <yehm@umich.edu>
2019-02-01 12:33:28 -0500

    Updated: AllWordsScreen to have swipe and delete functionality, did some styling

--- components/AllWordsScreen.js
+++ components/AllWordsScreen.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { ScrollView, StyleSheet, View, Text } from 'react-native';
+import { List, ListItem } from 'react-native-elements'
 import { Button } from 'react-native-elements';
+import Swipeout from 'react-native-swipeout'
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 import FlashCardScreen from './FlashCardScreen'
 import firebase from '../firebase';
+
 
 import { connect } from 'react-redux';
 import { updateWords } from '../store/words';
@@ -24,7 +27,7 @@ class AllWordsScreen extends Component {
   componentDidMount = async () => {
     const { uid } = await firebase.auth().currentUser;
     this.unsubscribe = await firebase.database().ref(`${uid}/spanish`).on('value', snapshot => {
-      const words = Object.values(snapshot.val());
+      const words = Object.values(snapshot.val() || {});
       this.props.updateWords(words);
     });
   }
@@ -36,23 +39,47 @@ class AllWordsScreen extends Component {
   }
 
   deleteWord = async (word) => {
-    console.log('word', word);
     const { uid } = await firebase.auth().currentUser;
     await firebase.database().ref(`${uid}/spanish/${word}`).remove();
   }
   
+
   render() {
+    const swipeBtns = word => ([
+      {
+      text: 'X',
+      backgroundColor: 'red',
+      underlayColor: 'rgba(0, 0, 0, 1, 0.6)',
+      onPress: () => { this.deleteWord(word.word) }
+      },
+      {
+        text: 'Learned',
+        backgroundColor: 'green'
+      }   
+    ]);
+
     return (
       <ScrollView style={styles.container}>
         <Button
           onPress={() => firebase.auth().signOut()}
           title="Log Out"
         />
-        {
-          this.props.words.map((word, idx) =>
-            <Text key={idx} onPress={() => {this.deleteWord(word.word)}}>{word.word}</Text>
-          )
-        }
+        <List containerStyle={{marginBottom: 20}}>
+          {
+            this.props.words.map((word, idx) => (
+            <Swipeout key={idx} right={swipeBtns(word)}>
+              <ListItem
+                hideChevron
+                titleNumberOfLines={0}
+                subtitleNumberOfLines={0}
+                key={idx}
+                title={word.word}
+                subtitle={word.translation}
+              />
+            </Swipeout>
+            ))
+          }
+          </List>    
       </ScrollView>
     );
   }
@@ -65,8 +92,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff"
   }
 });
-
-// connect(msp, mdp)(AllWordsScreen);
 
 const TabNavigator = createBottomTabNavigator({
   AllWords: connect(msp,mdp)(AllWordsScreen),
